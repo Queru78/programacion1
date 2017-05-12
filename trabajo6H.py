8c3b344bf480140dca1cd68be95b24292f0b1604
queru78 <querubinbicho@hotmail.com>
2017-05-11 23:22:34 -0300

    primer subida

---
+++ trabajo6H.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/python
+
+numero =input("ingrese numero ")
+
+el6= numero % 6
+if (el6==0):
+        print("el numero %s es divisible por 6"% (numero))
+else:
+        print("el numero %s no es divisible por 6" % (numero))
+
+
