118f4243ddb7ac1d19a55f3035f69d30ddf89ad0
queru78 <querubinbicho@hotmail.com>
2017-06-13 23:35:21 -0300

    nafta super

--- naftaclio.py
+++ naftaclio.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-
+#calcular de nafta para clio (localidad las flores)
 
 km = input ("ingrese la cantidad de kilometros a destino: ")
-nafta = (km*6/100)
+nafta = (km*6.0/100)
 
 print (("cantidad de litros a destino: %s litros ") %(nafta))
 gasto = (nafta*22)
