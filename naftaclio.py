5df1bb50f390e2b5f7988feaa373e536e2c1b79d
queru78 <querubinbicho@hotmail.com>
2017-06-13 22:07:11 -0300

    calcular la nafta del clio

---
+++ naftaclio.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+
+km = input ("ingrese la cantidad de kilometros a destino: ")
+nafta = (km*6/100)
+
+print (("cantidad de litros a destino: %s litros ") %(nafta))
+gasto = (nafta*22)
+
+print (("haciendo %s kilometros el clio gasta mas o menos: %s pesos ") % (km, gasto))
+
