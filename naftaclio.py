2a0954f777a46c0c2784c5d81739dbe7f9d2ea9b
queru78 <querubinbicho@hotmail.com>
2017-06-16 15:33:23 -0300

    Nafta super

--- naftaclio.py
+++ naftaclio.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 #calcular nafta para clio (localidad las flores)
 
@@ -8,4 +9,16 @@ print (("cantidad de litros a destino: %s litros ") %(nafta))
 gasto = (nafta*22)
 
 print (("haciendo %s kilometros el clio gasta mas o menos: %s pesos ") % (km, gasto))
+
+tiempo = (60*km/100)
+
+if (tiempo) <= 59:
+
+	print (("tiempo estimativo de llegada a destino: %s minutos") % (tiempo))
+
+hora = (tiempo/60.0)
+
+if (hora) >= 1:
+
+	print (("tiempo estimativo de llegada a destino: %s horas") % (hora))
 
